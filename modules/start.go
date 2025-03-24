2f12f4b778034f7a49862fc20aeaec35f9ae02fb
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-03-24 22:09:57 +0530

    Update start.go

--- modules/start.go
+++ modules/start.go
@@ -1,27 +1,33 @@
 package modules
 
 import (
+	"fmt"
 	"main/config"
-
 	tg "github.com/amarnathcjd/gogram/telegram"
 )
 
 func StartHandler(m *tg.NewMessage) error {
-	message := `Hello 👋, I'm your Edit Guardian Bot, 
+	user := m.GetSender()
+	mention := fmt.Sprintf(`<a href="tg://user?id=%d">%s</a>`, user.ID, user.FirstName)
+
+	message := fmt.Sprintf(`Hello %s 👋, I'm your Edit Guardian Bot, 
 here to maintain a secure environment for our discussions.
 
 🚫 Edited Message Deletion: I'll remove edited messages 
 to maintain transparency.
 
-📣 Notifications: You'll be informed each time a message is deleted.`
+📣 Notifications: You'll be informed each time a message is deleted.`, mention)
 
 	if config.StartImageUrl != "" {
 		_, err := m.ReplyMedia(config.StartImageUrl, tg.MediaOptions{
-			Caption: message,
+			Caption:   message,
+			ParseMode: "HTML",
 		})
 		return err
 	} else {
-		_, err := m.Reply(message)
+		_, err := m.Reply(message, tg.SendOptions{
+			ParseMode: "HTML",
+		})
 		return err
 	}
 }
