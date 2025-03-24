9c29d631ca0130eaa32ba9622538308e7689e67d
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-03-24 21:56:43 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -25,7 +25,7 @@ func init() {
 
 	APIHash = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
 	BotToken = getenv("BOT_TOKEN", "")
-	StartImageUrl = getenv("START_IMG_URL", "https://graph.org/file/d05729b62d49be693f360-7a214538921cf8c335.jpg")
+	StartImageUrl = getenv("START_IMG_URL", "https://graph.org/file/f3c8291963a053ac18536-3558d077ad80845bd7.jpg")
 }
 
 func getenv(key, defaultValue string) string {
