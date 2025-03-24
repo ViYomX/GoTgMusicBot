f29d706bac8ad48d5fd743b014d325c9a3fbb60e
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-03-24 21:52:26 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -19,13 +19,13 @@ func init() {
 
 	apiID, err := strconv.Atoi(getenv("API_ID", "0"))
 	if err != nil {
-		panic("Invalid API_ID: " + getenv("API_ID", ""))
+		panic("Invalid API_ID: " + getenv("API_ID", "12380656"))
 	}
 	APIID = int32(apiID)
 
-	APIHash = getenv("API_HASH", "")
+	APIHash = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
 	BotToken = getenv("BOT_TOKEN", "")
-	StartImageUrl = getenv("START_IMG_URL", "")
+	StartImageUrl = getenv("START_IMG_URL", "https://graph.org/file/d05729b62d49be693f360-7a214538921cf8c335.jpg")
 }
 
 func getenv(key, defaultValue string) string {
